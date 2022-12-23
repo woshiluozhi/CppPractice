5aa10c470275959353e9a0b3b37c81c427402d97
woshiluozhi <929887908@qq.com>
2022-12-23 22:42:28 +0800

    removeElements

--- 数据结构与算法/链表OJ/Test.c
+++ 数据结构与算法/链表OJ/Test.c
@@ -6,3 +6,32 @@ int main()
 
 	return 0;
 }
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode* dummyHead = malloc(sizeof(struct ListNode));
+    dummyHead->next = head;
+    struct ListNode* tmp = dummyHead;
+    while (tmp->next != NULL)
+    {
+        if (tmp->next->val == val)
+        {
+            tmp->next = tmp->next->next;
+        }
+        else
+        {
+            tmp = tmp->next;
+        }
+    }
+    return dummyHead->next;
+}
