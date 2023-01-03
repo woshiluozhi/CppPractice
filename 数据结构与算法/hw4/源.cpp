111742ba847bb5b1d91c1183bc13c738afd51e9c
woshiluozhi <929887908@qq.com>
2023-01-03 23:51:42 +0800

    //linux

--- 数据结构与算法/hw4/源.cpp
+++ 数据结构与算法/hw4/源.cpp
@@ -106,98 +106,140 @@
 ////}
 
 #include <stdio.h>
-#define a 10
-void foo();
+//#define a 10
+//void foo();
+//int main()
+//{
+//	printf("%d..", a);
+//	foo();
+//	printf("%d", a);
+//	return 0;
+//}
+//void foo() {
+//#undef a
+//#define a 50
+//}
+//
+//
+//#include <stdio.h>
+////#include <string.h>
+//
+//int main() {
+//    char str[1000];
+//    while (gets(str) != NULL) { // 注意 while 处理多个 case
+//        // 64 位输出请用 printf("%lld") to 
+//        int k = 0;
+//        int s = 0;
+//        int n = 0;
+//        int o = 0;
+//        for (int i = 0; i < strlen(str); i++)
+//        {
+//            if (str[i] >= '0' && str[i] <= '9')
+//                n++;
+//            else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+//                k++;
+//            else if (str[i] == ' ')
+//                s++;
+//            else
+//                o++;
+//        }
+//        printf("%d\n%d\n%d\n%d\n", k, s, n, o);
+//    }
+//    return 0;
+//}
+//
+//
+///**
+// * Note: The returned array must be malloced, assume caller calls free().
+// */
+//int* runningSum(int* nums, int numsSize, int* returnSize) {
+//    *returnSize = numsSize;
+//    for (int i = 1; i < numsSize; i++)
+//    {
+//        nums[i] += nums[i - 1];
+//    }
+//    return nums;
+//
+//}
+//
+//
+//int searchInsert(int* nums, int numsSize, int target) {
+//    int left = 0, right = numsSize - 1, ans = numsSize;
+//
+//    while (left <= right)
+//    {
+//        int mid = ((right - left) >> 1) + left;
+//        if (target <= nums[mid])
+//        {
+//            ans = mid;
+//            right = mid - 1;
+//        }
+//        else
+//        {
+//            left = mid + 1;
+//        }
+//    }
+//    return ans;
+//}
+//
+//
+///**
+// * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+// *
+// *
+// * @param n int整型 最大位数
+// * @return int整型一维数组
+// * @return int* returnSize 返回数组行数
+// */
+//int* printNumbers(int n, int* returnSize) {
+//    // write code here
+//    *returnSize = pow(10, n) - 1;
+//    int* arr = (int*)malloc(sizeof(int) * (*returnSize));
+//    for (int i = 0; i < *returnSize; i++)
+//    {
+//        arr[i] = i + 1;
+//    }
+//    return arr;
+//}
+
+//int i;
+//void prt()
+//{
+//	for (int i = 5; i < 8; i++) 
+//		printf("%c", '*');
+//	printf("\t");
+//} 
+//int main()
+//{
+//	for (int i = 5; i <= 8; i++)
+//		prt();
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	printf("%d\n", (a += a -= a * a));
+//	return 0;
+//}
+
 int main()
 {
-	printf("%d..", a);
-	foo();
-	printf("%d", a);
-	return 0;
-}
-void foo() {
-#undef a
-#define a 50
-}
-
-
-#include <stdio.h>
-//#include <string.h>
-
-int main() {
-    char str[1000];
-    while (gets(str) != NULL) { // 注意 while 处理多个 case
-        // 64 位输出请用 printf("%lld") to 
-        int k = 0;
-        int s = 0;
-        int n = 0;
-        int o = 0;
-        for (int i = 0; i < strlen(str); i++)
-        {
-            if (str[i] >= '0' && str[i] <= '9')
-                n++;
-            else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-                k++;
-            else if (str[i] == ' ')
-                s++;
-            else
-                o++;
-        }
-        printf("%d\n%d\n%d\n%d\n", k, s, n, o);
-    }
-    return 0;
+	char c;
+	int v0 = 0, v1 = 0, v2 = 0;
+		do
+		{
+			switch (c = getchar())
+			{
+			case'a':case'A':
+			case'e':case'E':
+			case'i':case'I':
+			case'o':case'O':
+			case'u':case'U':v1 += 1;
+			default: v0 += 1; v2 += 1;
+			}
+		} while (c != '\n');
+		printf("v0=%d,v1=%d,v2=%d\n", v0, v1, v2);
+		return 0;
 }
 
-
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* runningSum(int* nums, int numsSize, int* returnSize) {
-    *returnSize = numsSize;
-    for (int i = 1; i < numsSize; i++)
-    {
-        nums[i] += nums[i - 1];
-    }
-    return nums;
-
-}
-
-
-int searchInsert(int* nums, int numsSize, int target) {
-    int left = 0, right = numsSize - 1, ans = numsSize;
-
-    while (left <= right)
-    {
-        int mid = ((right - left) >> 1) + left;
-        if (target <= nums[mid])
-        {
-            ans = mid;
-            right = mid - 1;
-        }
-        else
-        {
-            left = mid + 1;
-        }
-    }
-    return ans;
-}
-
-
-/**
- * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
- *
- *
- * @param n int整型 最大位数
- * @return int整型一维数组
- * @return int* returnSize 返回数组行数
- */
-int* printNumbers(int n, int* returnSize) {
-    // write code here
-    *returnSize = pow(10, n) - 1;
-    int* arr = (int*)malloc(sizeof(int) * (*returnSize));
-    for (int i = 0; i < *returnSize; i++)
-    {
-        arr[i] = i + 1;
-    }
-    return arr;
-}
+//linux
