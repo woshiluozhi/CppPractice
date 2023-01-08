e0f8ee581dcaa208fb5ff31e539f0d7e94feb72b
woshiluozhi <929887908@qq.com>
2023-01-08 23:54:58 +0800

    提交测试代码

---
+++ Linux/linux/lesson1/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello Linux!\n");
+    printf("hello Linux!\n");
+    printf("hello Linux!\n");
+    printf("hello Linux!\n");
+    printf("hello Linux!\n");
+    return 0;
+}
