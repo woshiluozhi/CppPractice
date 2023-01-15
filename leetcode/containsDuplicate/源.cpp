295379e5349ab6b57b6ebcd89556b57944b3a111
woshiluozhi <929887908@qq.com>
2023-01-16 00:14:55 +0800

    存在重复元素

---
+++ leetcode/containsDuplicate/源.cpp
@@ -0,0 +1,32 @@
+#include "stdio.h"
+
+
+bool containsDuplicate(int* nums, int numsSize) {
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                printf("true");
+                return true;
+            }
+
+        }
+    }
+    printf("false");
+    return false;
+}
+
+int main()
+{
+    int arr[4] = { 1, 2, 3, 4 };
+    int numsSize = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < numsSize; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("numsSize = %d\n", numsSize);
+    containsDuplicate(arr, numsSize);
+}
