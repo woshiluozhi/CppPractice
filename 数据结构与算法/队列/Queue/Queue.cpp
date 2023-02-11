c06532be59c1e227cf302fca44b82d9c009e9ee3
woshiluozhi <929887908@qq.com>
2023-02-11 13:55:36 +0800

    队列Queue实现

--- 数据结构与算法/队列/Queue/Queue.cpp
+++ 数据结构与算法/队列/Queue/Queue.cpp
@@ -43,73 +43,43 @@ void QueuePush(Queue* pq, QDataType x)
 	}
 	pq->size++;
 }
-void QueuePop(Queue* pq);
-QDataType QueueFront(Queue* pq);
-QDataType QueueBack(Queue* pq);
-bool QueueEmpty(Queue* pq);
-int QueueSize(Queue* pq);
-
-//void QueuePop(Queue* pq)
-//{
-//	assert(pq);
-//	assert(!QueueEmpty(pq));
-//
-//	if (pq->head->next == NULL)
-//	{
-//		free(pq->head);
-//		pq->head = pq->tail = NULL;
-//	}
-//	else
-//	{
-//		QNode* del = pq->head;
-//		pq->head = pq->head->next;
-//
-//		free(del);
-//	}
-//
-//	pq->size--;
-//}
-//
-//QDataType QueueFront(Queue* pq)
-//{
-//	assert(pq);
-//	assert(!QueueEmpty(pq));
-//
-//	return pq->head->data;
-//}
-//
-//QDataType QueueBack(Queue* pq)
-//{
-//	assert(pq);
-//	assert(!QueueEmpty(pq));
-//
-//	return pq->tail->data;
-//}
-//
-//bool QueueEmpty(Queue* pq)
-//{
-//	assert(pq);
-//
-//	return pq->head == NULL && pq->tail == NULL;
-//}
-//
-//// 1G = 1024MB
-//// 1024MB = 1024*1024KB
-//// 1024*1024KB = 1024*1024*1024Byte
-//
-//int QueueSize(Queue* pq)
-//{
-//	assert(pq);
-//
-//	/*int size = 0;
-//	QNode* cur = pq->head;
-//	while (cur)
-//	{
-//		cur = cur->next;
-//		++size;
-//	}
-//
-//	return size;*/
-//
-//	return pq->size;
-//}
+bool QueueEmpty(Queue* pq)
+{
+	assert(pq);
+	return pq->head == nullptr && pq->tail == nullptr;
+}
+void QueuePop(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+	if (pq->head->next == nullptr)
+	{
+		free(pq->head);
+		pq->head = pq->tail = nullptr;
+	}
+	else
+	{
+		QNode* del = pq->head;
+		pq->head = pq->head->next;
+		free(del);
+	}
+	pq->size--;
+}
+QDataType QueueFront(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+	return pq->head->data;
+}
+QDataType QueueBack(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+	return pq->tail->data;
+}
+int QueueSize(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+	return pq->size;
+}
